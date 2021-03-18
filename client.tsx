1c67f273d77f3c2ed2bba131720d3dce7ff30910
Gyumong <rlaalsrb3559@naver.com>
2021-03-18 15:11:29 +0900

    숫자야구 tsx

--- client.tsx
+++ client.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {hot} from 'react-hot-loader/root';
-import WordRelay from './WordRelay';
+import NumberBaseball from './NumberBaseball';
 
-const Hot = hot(WordRelay); // HOC
 
-ReactDOM.render(<Hot/>,document.querySelector('#root'));
+
+ReactDOM.render(<NumberBaseball/>,document.querySelector('#root'));
